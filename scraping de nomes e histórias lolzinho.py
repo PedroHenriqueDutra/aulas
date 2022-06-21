8270e14d5d761bec29b8f32f5a344090b28b1889
Pedro <76980297+PedroHenriqueDutra@users.noreply.github.com>
2022-06-21 14:45:02 -0300

    Update scraping de nomes e histórias lolzinho.py

--- scraping de nomes e histórias lolzinho.py
+++ scraping de nomes e histórias lolzinho.py
@@ -1,15 +1,11 @@
-import time
-
-import time
-
 import requests
 from bs4 import  BeautifulSoup
-req = requests.get('https://www.leagueoflegends.com/pt-br/champions/')
+req = requests.get('https://ge.globo.com/esports/lol/noticia/2022/02/18/campeoes-do-lol-lista-completa-de-personagens-e-funcoes.ghtml')
 if req.status_code == 200:
             print('Requisição bem sucedida!')
             content = req.content
             soup = BeautifulSoup(content, 'html.parser')
-            table = soup.findAll( class_='style__Text-n3ovyt-3 gMLOLF')
+            table = soup.findAll(class_='show-table__content show-table__content--highlight-top')
             print('Os ultimos mangás que sairam hoje  no site https://www.supermangas.site/ são :\n')
             co = []
 
@@ -36,4 +32,3 @@ if req.status_code == 200:
 
 else:
             print('erro')
-
